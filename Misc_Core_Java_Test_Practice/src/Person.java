aaa32089e75ce72eda0883dc1def4ac73be2aae4
Vikas Jadhav <vikas-jadhav@king.com>
2023-06-20 23:53:09 +0530

    Testing Person class added for testing purpose

---
+++ Misc_Core_Java_Test_Practice/src/Person.java
@@ -0,0 +1,23 @@
+
+public class Person {
+
+	// class level member
+	StringBuffer firstName;
+	static StringBuffer lastName;
+
+	public String addExtra() {
+
+		// Local Variable
+		StringBuilder sb = new StringBuilder();
+		sb.append("Rahul").append(true);
+
+		return sb.toString();
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println(null instanceof null);
+	}
+
+}
